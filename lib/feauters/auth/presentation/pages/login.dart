401d8565d7ab44e057151e2a3e94a2e3c1e9ff39
yadishman <ynamssi5193@gmail.com>
2024-04-05 18:28:00 +0300

    navigation

--- lib/feauters/auth/presentation/pages/login.dart
+++ lib/feauters/auth/presentation/pages/login.dart
@@ -10,6 +10,23 @@ class LoginPage extends StatelessWidget{
         mainAxisAlignment:MainAxisAlignment.center, 
         children: [
         SizedBox(height: 20,),
+        Align(
+          alignment: Alignment.topLeft,
+          child: GestureDetector(
+            onTap: (){
+              Navigator.pushNamed(context, "/register");
+            },
+            child: Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.only(topRight:Radius.circular(25),bottomRight:
+               Radius.circular(25)), color: Colors.amber),
+              height: 50,
+              width: 120,
+              
+              child: Center(child: Text("Register", style: TextStyle(fontSize: 20, color: Colors.white),)),
+            ),
+          ),
+        ),
+        SizedBox(height: 30,),
         Icon(Icons.account_circle,size: 100,color: Colors.amber,),
         LoginForm(),
       ]),
--- lib/feauters/auth/presentation/pages/register.dart
+++ lib/feauters/auth/presentation/pages/register.dart
@@ -5,14 +5,32 @@ class RegisterPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-
-      body: Column(
-        mainAxisAlignment:MainAxisAlignment.center, 
-        children: [
-        SizedBox(height: 20,),
-        Icon(Icons.account_circle,size: 100,color: Colors.amber,),
-        RegisterForm(),
-      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center, 
+          children: [
+          SizedBox(height: 35,),
+          Align(
+            alignment: Alignment.topRight,
+            child: GestureDetector(
+              onTap: (){
+                Navigator.pushNamed(context, "/login");
+              },
+              child: Container(
+                decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft:Radius.circular(25),bottomLeft:
+                 Radius.circular(25)), color: Colors.amber),
+                height: 50,
+                width: 120,
+                
+                child: Center(child: Text("Log in", style: TextStyle(fontSize: 20, color: Colors.white),)),
+              ),
+            ),
+          ),
+          SizedBox(height: 15,),
+          Icon(Icons.account_circle,size: 100,color: Colors.amber,),
+          RegisterForm(),
+        ]),
+      ),
     );
   }
 
